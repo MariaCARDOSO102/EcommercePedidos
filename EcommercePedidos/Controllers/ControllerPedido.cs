fd55fa29f5de971b11983962847d32ae4c47adfa
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-04-28 17:21:40 -0300

    :sparkles: feat: Criando Controller.

---
+++ EcommercePedidos/Controllers/ControllerPedido.cs
@@ -0,0 +1,13 @@
+﻿using EcommercePedidos.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommercePedidos.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ControllerPedido : Controller
+    {
+        private readonly PedidoService _pedidoService;
+
+    }
+}
