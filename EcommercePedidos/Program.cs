e132dc069c9c6c990732920009ebc576b8b7432f
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-12 21:21:10 -0300

    :sparkles: feat: Configurando a Program.

--- EcommercePedidos/Program.cs
+++ EcommercePedidos/Program.cs
@@ -1,15 +1,24 @@
+using EcommercePedidos.Data;
+using EcommercePedidos.Data.Interfaces;
+using EcommercePedidos.Data.Repositories;
+using EcommercePedidos.Service.Interfaces;
+using EcommercePedidos.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IPedidoService, PedidoService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
